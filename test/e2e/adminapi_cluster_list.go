7ab1ee34cae1876108331ad908e14466fdaf264f
Angus Salkeld <asalkeld@redhat.com>
2020-07-02 11:51:18 +1000

    JMESPATH should only operate on the clusters not the nextPageLink
    This keeps the scope of the jmespath to ".value" to make sure that
    the nextPageLink is never removed by the filtering.

--- test/e2e/adminapi_cluster_list.go
+++ test/e2e/adminapi_cluster_list.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"strings"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -23,28 +24,8 @@ var _ = Describe("[Admin API] List clusters action", func() {
 		ctx := context.Background()
 		resourceID := resourceIDFromEnv()
 
-		// TODO: add pagination support once https://github.com/Azure/ARO-RP/pull/859 lands.
-		//       Replace code below with testAdminClustersList(...). See example below.
-		By("requesting the cluster document via RP admin API")
-		var ocs []*admin.OpenShiftCluster
-		resp, err := adminRequest(ctx, http.MethodGet, "/admin/providers/Microsoft.RedHatOpenShift/openShiftClusters", nil, nil, &ocs)
-		Expect(err).NotTo(HaveOccurred())
-		Expect(resp.StatusCode).To(Equal(http.StatusOK))
-
-		By("checking that we received the expected cluster")
-		var oc *admin.OpenShiftCluster
-		for i := range ocs {
-			if ocs[i].ID == resourceID {
-				oc = ocs[i]
-			}
-		}
-		Expect(oc).ToNot(BeNil())
-
-		By("checking that fields available only in Admin API have values")
-		// Note: some fields will have empty values
-		// on successfully provisioned cluster (oc.Properties.Install, for example)
-		Expect(oc.Properties.ServicePrincipalProfile.TenantID).ToNot(BeEmpty())
-		Expect(oc.Properties.StorageSuffix).ToNot(BeEmpty())
+		path := "/admin/providers/Microsoft.RedHatOpenShift/openShiftClusters"
+		testAdminClustersList(adminClustersList(ctx, path), resourceID)
 	})
 
 	It("should be able to return list clusters with admin fields by subscription", func() {
@@ -53,7 +34,7 @@ var _ = Describe("[Admin API] List clusters action", func() {
 		subscriptionID := os.Getenv("AZURE_SUBSCRIPTION_ID")
 
 		path := fmt.Sprintf("/subscriptions/%s/providers/Microsoft.RedHatOpenShift/openShiftClusters", subscriptionID)
-		testAdminClustersList(ctx, path, resourceID)
+		testAdminClustersList(adminClustersList(ctx, path), resourceID)
 	})
 
 	It("should be able to return list clusters with admin fields by resource group", func() {
@@ -63,14 +44,19 @@ var _ = Describe("[Admin API] List clusters action", func() {
 		resourceGroup := os.Getenv("RESOURCEGROUP")
 
 		path := fmt.Sprintf("/subscriptions/%s/resourceGroups/%s/providers/Microsoft.RedHatOpenShift/openShiftClusters", subscriptionID, resourceGroup)
-		testAdminClustersList(ctx, path, resourceID)
+		testAdminClustersList(adminClustersList(ctx, path), resourceID)
 	})
 })
 
-func testAdminClustersList(ctx context.Context, path, wantResourceID string) {
+func adminClustersList(ctx context.Context, path string) []*admin.OpenShiftCluster {
 	By("requesting the cluster document via RP admin API")
 	ocs := make([]*admin.OpenShiftCluster, 0)
 	params := url.Values{}
+
+	if !strings.HasPrefix(path, "/admin") {
+		params["api-version"] = []string{"admin"}
+	}
+
 	for {
 		var list admin.OpenShiftClusterList
 		resp, err := adminRequest(ctx, http.MethodGet, path, params, nil, &list)
@@ -85,7 +71,10 @@ func testAdminClustersList(ctx context.Context, path, wantResourceID string) {
 
 		params = nextParams(list.NextLink)
 	}
+	return ocs
+}
 
+func testAdminClustersList(ocs []*admin.OpenShiftCluster, wantResourceID string) {
 	By("checking that we received the expected cluster")
 	var oc *admin.OpenShiftCluster
 	for i := range ocs {
